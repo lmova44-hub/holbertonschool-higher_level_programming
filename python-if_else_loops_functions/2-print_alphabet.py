436acf125c36336628ce2f789fa695edfb48054a
lmova44-hub <lm.ova44@gmail.com>
2025-11-14 18:19:55 +0000

    Pycodestyle fix: Splitting print statement arguments (E501)

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i) for i in range(ord('a'), ord('z') + 1))), end="")
+print("{}".format("".join(chr(i) for i in range(ord('a'),
+                                             ord('z') + 1))), end="")
