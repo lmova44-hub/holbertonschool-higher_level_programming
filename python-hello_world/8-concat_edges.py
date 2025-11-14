6680f948265c1e29973d7715dd33f66243971b35
lmova44-hub <lm.ova44@gmail.com>
2025-11-14 17:37:22 +0000

    6. Create a new sentence: Final fix using print argument separation for spaces

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "object-oriented programming"
-str2 = "with"
-str3 = "Python"
-print(str1, str2, str3)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
