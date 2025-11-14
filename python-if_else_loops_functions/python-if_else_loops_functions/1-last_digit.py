b37902cc8575ce28199aca693a0e2e27137e020e
lmova44-hub <lm.ova44@gmail.com>
2025-11-14 20:02:51 +0000

    Final Fix: Ensured accurate negative last digit calculation using modulus operator (% -10).

--- python-if_else_loops_functions/python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = number % 10
-
-if number < 0 and last_digit != 0:
+if number < 0:
     last_digit = number % -10
+else:
+    last_digit = number % 10
 
 output_string = f"Last digit of {number} is {last_digit} and "
 
