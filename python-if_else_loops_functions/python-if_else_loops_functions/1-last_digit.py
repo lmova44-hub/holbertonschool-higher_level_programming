c52f8686cb5faeeb41fec51911de525153946c4d
lmova44-hub <lm.ova44@gmail.com>
2025-11-14 20:06:08 +0000

    Ultra-final Fix: Ensured accurate negative last digit calculation using number % -10.

--- python-if_else_loops_functions/python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
 if number < 0:
-    last_digit = number % -10
+    lastdigit = -((-number) % 10)
 else:
-    last_digit = number % 10
-
-output_string = f"Last digit of {number} is {last_digit} and "
-
-if last_digit > 5:
-    output_string += "is greater than 5"
-elif last_digit == 0:
-    output_string += "is 0"
+    lastdigit = number % 10
+if lastdigit > 5:
+    print("Last digit of", number, "is", lastdigit, "and is greater than 5")
+elif lastdigit == 0:
+    print("Last digit of", number, "is", lastdigit, "and is 0")
 else:
-    output_string += "is less than 6 and not 0"
-
-print(output_string)
+    print("Last digit of", number, "is", lastdigit,
+          "and is less than 6 and not 0")
