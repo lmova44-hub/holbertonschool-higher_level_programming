c503028b74b1a5cc3642f4c46b86c970b47e640f
lmova44-hub <lm.ova44@gmail.com>
2025-11-14 10:35:05 +0000

    Fix: Added missing quote character to 2-print.py output

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("Programming is like building a multilingual puzzle")
+# Write a Python script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+print("\"Programming is like building a multilingual puzzle")
