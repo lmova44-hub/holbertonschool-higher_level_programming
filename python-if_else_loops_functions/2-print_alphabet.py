6304d9b02109d2052d1b66f299497b5e40a84f47
lmova44-hub <lm.ova44@gmail.com>
2025-11-15 16:52:54 +0000

    2. Fixed pycodestyle E501 by shortening comments

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-# Yalnız bir dövrə və bir print funksiyası istifadə edərək əlifbanı çap edir.
-
-# Bütün hərfləri saxlamaq üçün boş sətir (string)
-# Bu, hərflərin dəyişəndə saxlanılması demək deyil, yalnız son çıxışı hazırlayır.
+# Əlifbanı çap etmək üçün bir dövrə və bir print istifadə olunur.
 alphabet = ""
 
-# 'a' hərfinin ASCII dəyərindən ('z' hərfinin ASCII dəyərinə qədər) dövr edirik.
-# ord('a') = 97, ord('z') = 122
+# ASCII dəyərləri üzərində dövr (97='a' dan 122='z' qədər)
 for i in range(ord('a'), ord('z') + 1):
-    # Rəqəmi simvola çevirib sətirə əlavə edirik
     alphabet += chr(i)
 
 # Bütün əlifbanı yeni sətir olmadan çap edirik
