5af63ca8104182205a6bd393348c4e39c5d136ce
lmova44-hub <lm.ova44@gmail.com>
2025-11-15 16:47:19 +0000

    1. The last digit (fixed negative modulo)

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,25 +2,24 @@
 import random
 number = random.randint(-10000, 10000)
 
-# Son rəqəmin hesablanması (nümunə çıxışlarına uyğun olaraq)
+# 1. Son rəqəmi tapın
 last_digit = number % 10
 
-# Mənfi ədədlərdə son rəqəmin işarəsini düzəltmək
-# Məsələn: -626 % 10 = 4. Nümunə: -6.
-if last_digit > 5 and number < 0:
+# 2. Mənfi ədədlər üçün korreksiya (nümunə çıxışı tələb edir ki, -98-in son rəqəmi -8 olsun)
+# number < 0 olarsa VƏ son rəqəm 0-a bərabər deyilsə (0 % 10 həmişə 0-dır və düzgündür)
+if number < 0 and last_digit != 0:
     last_digit -= 10
-# Qeyd: -9200 % 10 = 0 (düzgündür)
 
-# Çıxış sətrinin başlanğıcı
-output_string = "Last digit of {} is {}".format(number, last_digit)
+# 3. Şərtlərin yoxlanılması və çıxışın hazırlanması
+output_suffix = ""
 
-# Şərtlərin yoxlanılması
 if last_digit > 5:
-    output_string += " and is greater than 5"
+    output_suffix = " and is greater than 5"
 elif last_digit == 0:
-    output_string += " and is 0"
+    output_suffix = " and is 0"
+# last_digit < 6 AND last_digit != 0 üçün:
 elif last_digit < 6 and last_digit != 0:
-    output_string += " and is less than 6 and not 0"
+    output_suffix = " and is less than 6 and not 0"
 
 # Nəticəni çap etmək
-print(output_string)
+print("Last digit of {} is {}{}".format(number, last_digit, output_suffix))
