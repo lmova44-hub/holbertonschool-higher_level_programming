30d527a05300554b4640168bc988f514b1d88b3c
lmova44-hub <lm.ova44@gmail.com>
2025-11-15 16:51:20 +0000

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i) for i in range(ord('a'),
-                                             ord('z') + 1))), end="")
+# Yalnız bir dövrə və bir print funksiyası istifadə edərək əlifbanı çap edir.
+
+# Bütün hərfləri saxlamaq üçün boş sətir (string)
+# Bu, hərflərin dəyişəndə saxlanılması demək deyil, yalnız son çıxışı hazırlayır.
+alphabet = ""
+
+# 'a' hərfinin ASCII dəyərindən ('z' hərfinin ASCII dəyərinə qədər) dövr edirik.
+# ord('a') = 97, ord('z') = 122
+for i in range(ord('a'), ord('z') + 1):
+    # Rəqəmi simvola çevirib sətirə əlavə edirik
+    alphabet += chr(i)
+
+# Bütün əlifbanı yeni sətir olmadan çap edirik
+print("{}".format(alphabet), end="")
