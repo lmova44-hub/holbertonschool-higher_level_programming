d4fc87854ce4241caeea7b4fddb7e169e64f5ce3
lmova44-hub <lm.ova44@gmail.com>
2025-11-21 08:05:34 +0000

    dfasfsd

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    """ prints an int with .format """
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except (ValueError, TypeError):
         return False
