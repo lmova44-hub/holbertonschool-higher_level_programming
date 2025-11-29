4e4054f38fb65721eef251aff7cf13b35239d24d
lmova44-hub <lm.ova44@gmail.com>
2025-11-29 08:45:22 +0000

    weee

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python3
-"""Module 1-write_file: contains write_file function"""
-
-
 def write_file(filename="", text=""):
-    """Writes text to a UTF-8 file and returns the number of characters written"""
+    """Writes text to a UTF-8 file and returns
+    the number of characters written"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
