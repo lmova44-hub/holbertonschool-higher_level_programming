29f5095198adcf4ae380402bbdee8ef30ed0691d
lmova44-hub <lm.ova44@gmail.com>
2025-11-29 10:09:21 +0000

    jkdkjd

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Module 10-student: defines a Student class with filtered JSON representation"""
+"""Defines a Student class with filtered JSON representation."""
 
 
 class Student:
-    """Defines a student with first_name, last_name, and age"""
+    """Defines a student with first_name, last_name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize Student with first_name, last_name, and age"""
+        """Initialize Student with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return dictionary representation of Student instance
+        """Return dictionary representation of Student instance.
 
         If attrs is a list of strings, only include attributes in that list.
         Otherwise, include all attributes.
