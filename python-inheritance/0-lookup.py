32d3ca65b9998c79c9e65ae8784834532c8d3668
lmova44-hub <lm.ova44@gmail.com>
2025-11-29 05:00:16 +0000

    jskwkw

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,6 +4,7 @@ Module 0-lookup
 Contains a function that returns available attributes and methods of an object
 """
 
+
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object
