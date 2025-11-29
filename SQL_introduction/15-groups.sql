dd6c4a7a0e57f567e5081a7a628227ecc6642c77
lmova44-hub <lm.ova44@gmail.com>
2025-11-29 13:30:23 +0000

    dnkjwd

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- joiwdejidiwjoiw
 
-SELECT score , COUNT(*) AS number FROM second_table GROUP BY score ORDER BY DESC number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
