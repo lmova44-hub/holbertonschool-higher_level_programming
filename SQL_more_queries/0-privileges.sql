29aadb474b57db856b8f4d5a0c50896066d44a01
lmova44-hub <lm.ova44@gmail.com>
2025-11-29 14:00:51 +0000

    kjwe

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,7 @@
--- salam
+-- Create the users if they don't exist, but do NOT grant any privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
-FLUSH PRIVILEGES;
-
+-- Show grants for each user
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
