ba7030b9f266cda8d7b93b48ec76d40b7c3d3908
lmova44-hub <lm.ova44@gmail.com>
2025-11-29 11:32:25 +0000

    kjw

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,13 +1,15 @@
 -- Now you're back
 -- From outer space
+
 CREATE TABLE IF NOT EXISTS second_table (
-   id INT,
-   name  VARCHAR(256),
-   score INT
+    id INT,
+    name VARCHAR(256),
+    score INT
 );
 
-INSERT INTO second_table (id,name,score) VALUES  
-(1,"John",10)
-(2,"Alex",3)
-(3,"Bob",14)
-(4,"George",8)
+-- Insert multiple rows
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
