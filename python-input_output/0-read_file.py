01cf62f5d3e76b0bb13188bfe0beee9ed6954251
lmova44-hub <lm.ova44@gmail.com>
2025-11-29 08:23:45 +0000

    ejkee

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module 0-read_file: contains read_file function"""
 
+
 def read_file(filename=""):
     """Reads a text file and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
