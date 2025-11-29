a5f3b6ae2055799ab33d54695d44d3db122085a7
lmova44-hub <lm.ova44@gmail.com>
2025-11-29 13:58:39 +0000

    ww

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,11 @@
+-- salam
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
