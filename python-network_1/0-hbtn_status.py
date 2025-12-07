71bb26833713ae0a27b6b406de8624c4b5b20911
lmova44-hub <lm.ova44@gmail.com>
2025-12-07 13:51:24 +0000

    jwsk

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-""" What's my status? tapmaliyiq """
+"""
+Fetches https://intranet.hbtn.io/status using urllib
+and displays the response body with type and content
+"""
 
 from urllib import request
-url="https://intranet.hbtn.io/status"
-response=request.urlopen(url)
-print(response.read())
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    with request.urlopen(url) as response:
+        body = response.read()  # cavabı bytes formatında alırıq
+
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
